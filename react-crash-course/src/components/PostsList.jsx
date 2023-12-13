f261f875641ba2e6309d6614fc05aa1b5d18790a
Lucas Ribeiro <lucas.daniel@gmail.com>
2023-12-13 15:25:22 -0500

    fetching data

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -1,11 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostsList({ isPosting, onStopPosting }) {
+  // when you update state the componant gets executed again by React, so this fetch may
+  // will catch in a infinite loop. For this issue we use useEffect Hok
+
   const [posts, setPosts] = useState([]);
+
+  // the second argument (array) we will control when this function gets executed
+  // the array specifies the dependencies, when this dependencies (variables or functions)
+  // has their values changed the useEffect function will be executed
+  // if this argumento is empty it means that the useEffect function gets executed only once
+  // when the component is first rendered
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
     fetch("http://localhost:8080/posts", {
@@ -15,7 +32,7 @@ function PostsList({ isPosting, onStopPosting }) {
         "Content-Type": "application/json",
       },
     });
-    
+
     // when we want to update a state and the new state is based on the previous state
     // we need to pass a function
     setPosts((existingPosts) => [postData, ...existingPosts]);
