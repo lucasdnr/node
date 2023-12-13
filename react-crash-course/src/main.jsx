398491ab2de4b188e9e70ad458bb0ce6999c0783
Lucas Ribeiro <lucas.daniel@gmail.com>
2023-12-13 15:46:46 -0500

    adding routes

--- react-crash-course/src/main.jsx
+++ react-crash-course/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App";
+import "./index.css";
+import NewPost from "./components/NewPost";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  }, // our-domain
+  {
+    path: "/create-post",
+    element: <NewPost />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
